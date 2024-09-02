5383e28a8fd16b08e0df20eae155e48bbb93f0bb
Salahuddin101 <118743075+Salahuddin101@users.noreply.github.com>
2024-09-02 06:16:44 +0500

    Update app.mjs
    aa

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ import moment from 'moment-timezone';
 import { MongoClient } from 'mongodb';
 
 
-const mongoUrl = process.env.MONGO_URL;
+const mongoUrl = process.env.MONGO_URL || "aaa";
 const mongoClient = new MongoClient(mongoUrl);
 
 
